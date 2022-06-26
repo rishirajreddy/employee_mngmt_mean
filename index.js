066519ec81150f5334f80ffb468916670b88267b
rishirajreddy <rishirajreddy.07@gmail.com>
2022-06-27 01:01:19 +0530

    added 404 route

--- index.js
+++ index.js
@@ -13,12 +13,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 app.use('/employees', routes);
 
+
 app.use(express.static(__dirname+'/angular_src/dist/employee-mngmt'))
 
 app.get('/*',(req,res) => {
     res.sendFile(path.join(__dirname+'/angular_src/dist/employee-mngmt/index.html'))
 })
 
+app.use((req, res, next) => {
+    const error = new Error("Nothing Found!! Check your app again..");
+    error.status = 404;
+    next(error);
+  });
 
 
 app.listen(port,() => {
