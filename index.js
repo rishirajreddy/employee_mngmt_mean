4e860ef68fe677e37bd1ec0de81df73f493c6060
rishirajreddy <rishirajreddy.07@gmail.com>
2022-06-27 19:24:05 +0530

    added new build folder

--- index.js
+++ index.js
@@ -8,18 +8,16 @@ const port = process.env.PORT || 5000;
 const path = require('path');
 
 db();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors());
 app.use('/employees', routes);
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname+'/angular_src/dist/employee-mngmt')))
+app.use(express.static(path.join(__dirname+'/public')))
     
-    app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname+'/angular_src/dist/employee-mngmt/index.html'))
+app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname+'/public/index.html'))
     })
-}
 
 app.use((req, res, next) => {
     const error = new Error("Nothing Found!! Check your app again..");
